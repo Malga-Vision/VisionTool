b07494159df0530c885e8bfe1d25355310672668
VitoPaoloPastore <51142446+VitoPaoloPastore@users.noreply.github.com>
2020-12-16 12:59:56 +0100

    Update auxfun_drag_label.py

--- VisionTool/auxfun_drag_label.py
+++ VisionTool/auxfun_drag_label.py
@@ -1,4 +1,25 @@
-#Inherited from DeepLabCUT
+"""
+    Source file name: nb_panel.py  
+    
+    Description: this file contains the code to handle the GUI for selecting new videos and opening existing projects
+    
+    #Inherited from DeepLabCUT 
+    Alexander Mathis, Pranav Mamidanna, Kevin M Cury, Taiga Abe, Venkatesh N Murthy,Mackenzie Weygandt Mathis, and Matthias Bethge. Deeplabcut: markerless pose estimation
+    of user-defined body parts with deep learning. Nature neuroscience, 21(9):1281{1289}, 2018.
+    
+    Copyright (C) <2020>  <Vito Paolo Pastore, Matteo Moro, Francesca Odone>
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3 of the License.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+
 
 import numpy as np
 import wx
