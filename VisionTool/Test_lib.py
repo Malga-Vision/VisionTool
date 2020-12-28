10535bf3b0c3bc31043a3ba8cedc2807d461f553
VitoPaoloPastore <51142446+VitoPaoloPastore@users.noreply.github.com>
2020-12-29 00:46:24 +0100

    Update Test_lib.py

--- VisionTool/Test_lib.py
+++ VisionTool/Test_lib.py
@@ -80,7 +80,6 @@ class test ():
             else:
                 self.upload_new_Videos()
 
-            Frame_selection(None, os.path.dirname(self.config_file_text) + os.sep + 'annotation_options.txt');
 
     def upload_new_Videos(self):
 
