9cb368b1b74eb83379c703d1b03bf62fd493f8a6
VitoPaoloPastore <51142446+VitoPaoloPastore@users.noreply.github.com>
2021-01-09 02:43:49 +0100

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,8 +1,8 @@
 """
     Source file name: test.py
-    
+
     Description: this file contains the code to perform tests operations
-    
+
     Copyright (C) <2020>  <Vito Paolo Pastore, Matteo Moro, Francesca Odone>
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -15,45 +15,50 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-
-
-
 from VisionTool.Test_lib import test
-
-# initialize the test object
 
 test = test()
 
-# create a new project
+'''initialize the test object from the source file 'Test_lib'. The only purpose of 'Test_lib' is to provide a fast example of working flow for VisionTool out of the 
+regular operation which would be performed totally using the provided GUI (in source file 'main.py'). 
+Besides the tests, for regular usage, please use the module 'main' as described in the tutorial and file readme. '''
+
 
 test.new_project_routine()
 
-# or open an existing one  (choose one or the other)
+# create a new project
 
 test.open_project()
 
-# add videos to the project, or load the ones already added
+# or open an existing one  (choose one or the other, you can comment one of the two lines of code)
 
 test.Load_Videos()
 
-# set preferences for annotation
+# add videos to the project, or load the ones already added, for test, you can use the file 'sample_video.avi' downloaded with the repository 
 
-Frame_selection(None, os.path.dirname(test.config_file_text) + os.sep + 'annotation_options.txt')
+#test.Frame_selection()
 
-# if you are using the sample_video uploaded in the repository, use the next line of code to download existing annotation for testing
+# set preferences for annotation (not necessary if you use the line of code load_testing_annotation, uncomment if you want to make your annotation again). 
+
+
 test.load_testing_annotation(test.config_file_text)
 
-# needed for proceeding with manual or automatic annotation, code to handle the rest of operations
-
-test.annotate(0,test.config_file_text)
-
-# set the preferences for the neural network prediction
+# if you are using the sample_video uploaded in the repository, use the next line of code to download existing annotation for training and testing
 
 test.preferences_annotation()
+# set the preferences for the neural network prediction
+
+test.annotate(0, test.config_file_text)
+
+# needed for proceeding with manual or automatic annotation, code to handle the rest of operations.
+# you can view the annotation, and more important, this line of code extracts the frames. 
 
 # perform training
 test.check_and_train()
 
-# view results
+# use 
+#test.check_and_test() to perform testing if a neural network has already been trained.
+
 test.view_annotation()
 
+# view results of prediction!
